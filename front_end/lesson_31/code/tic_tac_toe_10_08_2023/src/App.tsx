7254e3a07d1ff77b3a5f69db6dd367c93d90d425
IGOR_EIDELMAN <v1148656@gmail.com>
2023-08-11 14:54:27 +0300

    Add lesson_31

---
+++ front_end/lesson_31/code/tic_tac_toe_10_08_2023/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="app">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
